e4482e41a67b22149395b7fb202942b096e9d7a5
Chulwoo Park <park@chulwoo.dev>
2020-12-27 00:03:57 +0900

    feat(page): add page model

---
+++ lib/common/model/page.dart
@@ -0,0 +1,40 @@
+import 'dart:collection';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+
+class Page<T> with ListMixin<T> implements List<T> {
+  const Page({
+    @required this.page,
+    @required this.totalCount,
+    @required List<T> items,
+  }) : this._items = items;
+
+  final int page;
+  final int totalCount;
+  final List<T> _items;
+
+  @override
+  int get length => _items.length;
+  set length(value) {
+    _items.length = value;
+  }
+
+  @override
+  T operator [](int index) => _items[index];
+
+  @override
+  void operator []=(int index, T value) => _items[index] = value;
+
+  @override
+  int get hashCode => hashValues(page, totalCount, _items);
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other is Page<T> &&
+            page == other.page &&
+            totalCount == other.totalCount &&
+            listEquals(_items, other._items));
+  }
+}
