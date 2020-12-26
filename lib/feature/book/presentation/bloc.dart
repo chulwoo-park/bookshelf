98564afb41afa8f692dc1ab66841563178418fa8
Chulwoo Park <park@chulwoo.dev>
2020-12-26 16:51:09 +0900

    feat(book): implement book list bloc

--- lib/feature/book/presentation/bloc.dart
+++ lib/feature/book/presentation/bloc.dart
@@ -9,8 +9,76 @@ class BookListBloc extends Bloc<BookListEvent, BookListState> {
   final SearchUseCase _search;
 
   @override
-  Stream<BookListState> mapEventToState(BookListEvent event) {
-    // TODO: implement mapEventToState
-    throw UnimplementedError();
+  Stream<BookListState> mapEventToState(BookListEvent event) async* {
+    if (event is BookSearched) {
+      yield* _mapSearchEvent(event);
+    } else if (event is NextPageRequested) {
+      yield* _mapNextPageRequestedEvent(event);
+    }
+  }
+
+  Stream<BookListState> _mapSearchEvent(BookSearched event) async* {
+    if (state is Loading) {
+      return;
+    }
+
+    yield Loading();
+    try {
+      final result = await _search.execute(
+        SearchParam(
+          event.query,
+          page: 1,
+        ),
+      );
+      yield Success(
+        List.unmodifiable(result),
+        query: event.query,
+        page: 1,
+      );
+    } catch (e, s) {
+      yield Failure(e, s);
+    }
+  }
+
+  Stream<BookListState> _mapNextPageRequestedEvent(
+    NextPageRequested event,
+  ) async* {
+    if (this.state is! Success) {
+      return;
+    }
+
+    final Success state = this.state;
+    if (state.loadMoreState == BookListLoadMoreState.loading) {
+      return;
+    }
+
+    yield state.copyWith(loadMoreState: BookListLoadMoreState.loading);
+
+    try {
+      final result = await _search.execute(
+        SearchParam(
+          state.query,
+          page: state.page + 1,
+        ),
+      );
+
+      var loadMoreState;
+      if (result.isEmpty) {
+        loadMoreState = BookListLoadMoreState.reachedEnd;
+      } else {
+        loadMoreState = BookListLoadMoreState.idle;
+      }
+      yield state.copyWith(
+        data: List.unmodifiable(
+          state.data + result,
+        ),
+        page: state.page + 1,
+        loadMoreState: loadMoreState,
+      );
+    } catch (e) {
+      yield state.copyWith(
+        loadMoreState: BookListLoadMoreState.failure,
+      );
+    }
   }
 }
--- lib/feature/book/presentation/state.dart
+++ lib/feature/book/presentation/state.dart
@@ -1,19 +1,81 @@
 import 'package:bookshelf/feature/book/domain/model.dart';
+import 'package:flutter/foundation.dart';
 
-abstract class BookListState {}
+abstract class BookListState {
+  const BookListState();
 
-class Initial implements BookListState {}
+  @override
+  int get hashCode => runtimeType.hashCode;
 
-class Loading implements BookListState {}
-
-class Success implements BookListState {
-  const Success(this.data, this.loadMoreState);
-
-  final List<Book> data;
-  final BookListLoadMoreState loadMoreState;
+  @override
+  bool operator ==(Object other) => runtimeType == other.runtimeType;
 }
 
-class Failure implements BookListState {}
+class Initial extends BookListState {
+  const Initial();
+}
+
+class Loading extends BookListState {
+  const Loading();
+}
+
+class Success extends BookListState {
+  const Success(
+    this.data, {
+    @required this.query,
+    this.page = 1,
+    this.loadMoreState = BookListLoadMoreState.idle,
+  });
+
+  final String query;
+  final int page;
+  final List<Book> data;
+  final BookListLoadMoreState loadMoreState;
+
+  @override
+  int get hashCode {
+    return 31 +
+        data.hashCode +
+        query.hashCode +
+        page.hashCode +
+        loadMoreState.hashCode;
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other is Success &&
+            listEquals(data, other.data) &&
+            query == other.query &&
+            page == other.page &&
+            loadMoreState == other.loadMoreState);
+  }
+
+  Success copyWith({
+    List<Book> data,
+    int page,
+    BookListLoadMoreState loadMoreState,
+  }) {
+    return Success(
+      data ?? this.data,
+      query: this.query,
+      page: page ?? this.page,
+      loadMoreState: loadMoreState ?? this.loadMoreState,
+    );
+  }
+
+  @override
+  String toString() {
+    return '{data: $data, query: $query, page: $page, loadMoreState: $loadMoreState}';
+  }
+}
+
+class Failure implements BookListState {
+  const Failure(this.error, [this.stackTrace]);
+
+  final Object error;
+  final StackTrace stackTrace;
+}
 
 enum BookListLoadMoreState {
   idle,
