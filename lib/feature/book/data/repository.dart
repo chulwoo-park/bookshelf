65732ebbe162683ad015a91798f3a66ede18a2b7
Chulwoo Park <park@chulwoo.dev>
2020-12-27 13:01:28 +0900

    feat(note): implement note repository

--- lib/feature/book/data/repository.dart
+++ lib/feature/book/data/repository.dart
@@ -31,3 +31,20 @@ class BookRepositoryImpl implements BookRepository {
             }));
   }
 }
+
+class NoteRepositoryImpl implements NoteRepository {
+  const NoteRepositoryImpl(this._localSource);
+
+  final LocalNoteSource _localSource;
+
+  @override
+  Future<Note> create(String isbn, String contents) {
+    final note = Note(isbn, contents);
+    return _localSource.add(note).then((_) => note);
+  }
+
+  @override
+  Future<List<Note>> getList(String isbn) {
+    return _localSource.getList(isbn);
+  }
+}
